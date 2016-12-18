5e58fd982147f58f02a9a69ca54205363f3bfb74
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-12-18 11:20:07 -0800

    Make tests work.

--- test.sh
+++ test.sh
@@ -17,18 +17,22 @@ export SOFTHSM2_CONF=${DIR}/softhsm.conf
 echo directories.tokendir = ${DIR} > ${SOFTHSM2_CONF}
 softhsm2-util --slot 0 --init-token --label silly_signer --pin 1234 --so-pin 5678
 softhsm2-util --slot 0 --import testdata/silly_signer.key --label silly_signer_key --pin 1234 --id F00D
-softhsm2-util --slot 0 --init-token --label entropic_ecdsa --pin 1234 --so-pin 5678
-softhsm2-util --slot 0 --import testdata/entropic_ecdsa.key --label entropic_ecdsa_key --pin 1234 --id C0FFEE
+softhsm2-util --slot 1 --init-token --label entropic_ecdsa --pin 1234 --so-pin 5678
+softhsm2-util --slot 1 --import testdata/entropic_ecdsa.key --label entropic_ecdsa_key --pin 1234 --id C0FFEE
 
 MODULE=/usr/lib/x86_64-linux-gnu/softhsm/libsofthsm2.so
 
+go test github.com/letsencrypt/pkcs11key
+
 go test github.com/letsencrypt/pkcs11key -module ${MODULE} \
+  -test.run xxxNONExxx \
   -pin 1234 -tokenLabel "silly_signer" \
   -cert testdata/silly_signer.pem \
   -test.bench Bench \
   -sessions 2
 
 go test github.com/letsencrypt/pkcs11key -module ${MODULE} \
+  -test.run xxxNONExxx \
   -pin 1234 -tokenLabel "entropic_ecdsa" \
   -cert testdata/entropic_ecdsa.pem \
   -test.bench Bench \
