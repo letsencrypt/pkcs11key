1f4ce303c3f4b90105bd2563394c759e243074b0
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2017-10-19 22:59:26 -0700

    Change to use p11.

--- key.go
+++ key.go
@@ -16,6 +16,7 @@ import (
 	"sync"
 
 	"github.com/miekg/pkcs11"
+	"github.com/miekg/pkcs11/p11"
 )
 
 // from src/pkg/crypto/rsa/pkcs1v15.go
@@ -45,23 +46,7 @@ var curveOIDs = map[string]asn1.ObjectIdentifier{
 	"P-521": oidNamedCurveP521,
 }
 
-// ctx defines the subset of pkcs11.ctx's methods that we use, so we can inject
-// a different ctx for testing.
-type ctx interface {
-	CloseSession(sh pkcs11.SessionHandle) error
-	FindObjectsFinal(sh pkcs11.SessionHandle) error
-	FindObjectsInit(sh pkcs11.SessionHandle, temp []*pkcs11.Attribute) error
-	FindObjects(sh pkcs11.SessionHandle, max int) ([]pkcs11.ObjectHandle, bool, error)
-	GetAttributeValue(sh pkcs11.SessionHandle, o pkcs11.ObjectHandle, a []*pkcs11.Attribute) ([]*pkcs11.Attribute, error)
-	GetSlotList(tokenPresent bool) ([]uint, error)
-	GetTokenInfo(slotID uint) (pkcs11.TokenInfo, error)
-	Initialize() error
-	Login(sh pkcs11.SessionHandle, userType uint, pin string) error
-	Logout(sh pkcs11.SessionHandle) error
-	OpenSession(slotID uint, flags uint) (pkcs11.SessionHandle, error)
-	SignInit(sh pkcs11.SessionHandle, m []*pkcs11.Mechanism, o pkcs11.ObjectHandle) error
-	Sign(sh pkcs11.SessionHandle, message []byte) ([]byte, error)
-}
+var appMu sync.Mutex
 
 // Key is an implementation of the crypto.Signer interface using a key stored
 // in a PKCS#11 hardware token.  This enables the use of PKCS#11 tokens with
@@ -81,7 +66,7 @@ type ctx interface {
 // to login repeatedly with an incorrect PIN, locking the PKCS#11 token.
 type Key struct {
 	// The PKCS#11 library to use
-	module ctx
+	module p11.Module
 
 	// The label of the token to be used (mandatory).
 	// We will automatically search for this in the slot list.
@@ -93,60 +78,21 @@ type Key struct {
 	// The public key corresponding to the private key.
 	publicKey crypto.PublicKey
 
-	// The an ObjectHandle pointing to the private key on the HSM.
-	privateKeyHandle pkcs11.ObjectHandle
+	// An handle representing the private key on the HSM.
+	privateKey p11.PrivateKey
 
 	// A handle to the session used by this Key.
-	session   *pkcs11.SessionHandle
-	sessionMu sync.Mutex
+	session p11.Session
 
 	// True if the private key has the CKA_ALWAYS_AUTHENTICATE attribute set.
 	alwaysAuthenticate bool
 }
 
-var modules = make(map[string]ctx)
-var modulesMu sync.Mutex
-
-// initialize loads the given PKCS#11 module (shared library) if it is not
-// already loaded. It's an error to load a PKCS#11 module multiple times, so we
-// maintain a map of loaded modules. Note that there is no facility yet to
-// unload a module ("finalize" in PKCS#11 parlance). In general, modules will
-// be unloaded at the end of the process.  The only place where you are likely
-// to need to explicitly unload a module is if you fork your process after a
-// Key has already been created, and the child process also needs to use
-// that module.
-func initialize(modulePath string) (ctx, error) {
-	modulesMu.Lock()
-	defer modulesMu.Unlock()
-	module, ok := modules[modulePath]
-	if ok {
-		return module, nil
-	}
-
-	newModule := pkcs11.New(modulePath)
-	if newModule == nil {
-		return nil, fmt.Errorf("failed to load module '%s'", modulePath)
-	}
-
-	err := newModule.Initialize()
-	if err != nil {
-		return nil, fmt.Errorf("failed to initialize module: %s", err)
-	}
-
-	modules[modulePath] = ctx(newModule)
-
-	return ctx(newModule), nil
-}
-
 // New instantiates a new handle to a PKCS #11-backed key.
 func New(modulePath, tokenLabel, pin string, publicKey crypto.PublicKey) (*Key, error) {
-	module, err := initialize(modulePath)
+	module, err := p11.OpenModule(modulePath)
 	if err != nil {
 		return nil, fmt.Errorf("pkcs11key: %s", err)
-	}
-	if module == nil {
-		err = fmt.Errorf("pkcs11key: nil module")
-		return nil, err
 	}
 
 	// Initialize a partial key
@@ -164,143 +110,116 @@ func New(modulePath, tokenLabel, pin string, publicKey crypto.PublicKey) (*Key, 
 	return ps, nil
 }
 
-// findObject finds an object in the PKCS#11 token according to a template. It
-// returns error if there is not exactly one result, or if there was an error
-// during the find calls. It must be called with the ps.sessionMu lock held.
-func (ps *Key) findObject(template []*pkcs11.Attribute) (pkcs11.ObjectHandle, error) {
-	if err := ps.module.FindObjectsInit(*ps.session, template); err != nil {
-		return 0, err
+func templateFromRSAPublic(key *rsa.PublicKey) []*pkcs11.Attribute {
+	return []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
+		pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_RSA),
+		pkcs11.NewAttribute(pkcs11.CKA_MODULUS, key.N.Bytes()),
+		pkcs11.NewAttribute(pkcs11.CKA_PUBLIC_EXPONENT, big.NewInt(int64(key.E)).Bytes()),
 	}
+}
 
-	handles, moreAvailable, err := ps.module.FindObjects(*ps.session, 1)
+func templateFromECDSAPublic(key *ecdsa.PublicKey) ([]*pkcs11.Attribute, error) {
+	// http://docs.oasis-open.org/pkcs11/pkcs11-curr/v2.40/os/pkcs11-curr-v2.40-os.html#_ftn1
+	// PKCS#11 v2.20 specified that the CKA_EC_POINT was to be store in a DER-encoded
+	// OCTET STRING.
+	rawValue := asn1.RawValue{
+		Tag:   4, // in Go 1.6+ this is asn1.TagOctetString
+		Bytes: elliptic.Marshal(key.Curve, key.X, key.Y),
+	}
+	marshalledPoint, err := asn1.Marshal(rawValue)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
-	if moreAvailable {
-		return 0, errors.New("too many objects returned from FindObjects")
+	curveOID, err := asn1.Marshal(curveOIDs[key.Curve.Params().Name])
+	if err != nil {
+		return nil, err
 	}
-	if err = ps.module.FindObjectsFinal(*ps.session); err != nil {
-		return 0, err
-	} else if len(handles) == 0 {
-		return 0, errors.New("no objects found")
-	}
-	return handles[0], nil
+	return []*pkcs11.Attribute{
+		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
+		pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_EC),
+		pkcs11.NewAttribute(pkcs11.CKA_EC_PARAMS, curveOID),
+		pkcs11.NewAttribute(pkcs11.CKA_EC_POINT, marshalledPoint),
+	}, nil
 }
 
 // getPublicKeyID looks up the given public key in the PKCS#11 token, and
 // returns its ID as a []byte, for use in looking up the corresponding private
-// key. It must be called with the ps.sessionMu lock held.
+// key.
 func (ps *Key) getPublicKeyID(publicKey crypto.PublicKey) ([]byte, error) {
 	var template []*pkcs11.Attribute
+	var err error
 	switch key := publicKey.(type) {
 	case *rsa.PublicKey:
-		template = []*pkcs11.Attribute{
-			pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
-			pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_RSA),
-			pkcs11.NewAttribute(pkcs11.CKA_MODULUS, key.N.Bytes()),
-			pkcs11.NewAttribute(pkcs11.CKA_PUBLIC_EXPONENT, big.NewInt(int64(key.E)).Bytes()),
-		}
+		template = templateFromRSAPublic(key)
 	case *ecdsa.PublicKey:
-		// http://docs.oasis-open.org/pkcs11/pkcs11-curr/v2.40/os/pkcs11-curr-v2.40-os.html#_ftn1
-		// PKCS#11 v2.20 specified that the CKA_EC_POINT was to be store in a DER-encoded
-		// OCTET STRING.
-		rawValue := asn1.RawValue{
-			Tag:   4, // in Go 1.6+ this is asn1.TagOctetString
-			Bytes: elliptic.Marshal(key.Curve, key.X, key.Y),
-		}
-		marshalledPoint, err := asn1.Marshal(rawValue)
+		template, err = templateFromECDSAPublic(key)
 		if err != nil {
 			return nil, err
-		}
-		curveOID, err := asn1.Marshal(curveOIDs[key.Curve.Params().Name])
-		if err != nil {
-			return nil, err
-		}
-		template = []*pkcs11.Attribute{
-			pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PUBLIC_KEY),
-			pkcs11.NewAttribute(pkcs11.CKA_KEY_TYPE, pkcs11.CKK_EC),
-			pkcs11.NewAttribute(pkcs11.CKA_EC_PARAMS, curveOID),
-			pkcs11.NewAttribute(pkcs11.CKA_EC_POINT, marshalledPoint),
 		}
 	default:
 		return nil, fmt.Errorf("unsupported public key of type %T", publicKey)
 	}
 
-	publicKeyHandle, err := ps.findObject(template)
+	p11PK, err := ps.session.FindObject(template)
 	if err != nil {
 		return nil, err
 	}
 
-	attrs, err := ps.module.GetAttributeValue(*ps.session, publicKeyHandle, []*pkcs11.Attribute{
-		pkcs11.NewAttribute(pkcs11.CKA_ID, nil),
-	})
+	pkID, err := p11PK.Attribute(pkcs11.CKA_ID)
 	if err != nil {
 		return nil, err
 	}
-	if len(attrs) > 0 && attrs[0].Type == pkcs11.CKA_ID {
-		return attrs[0].Value, nil
-	}
-	return nil, fmt.Errorf("invalid result from GetAttributeValue")
+	return pkID, nil
 }
 
 func (ps *Key) setup() error {
 	// Open a session
-	ps.sessionMu.Lock()
-	defer ps.sessionMu.Unlock()
 	session, err := ps.openSession()
 	if err != nil {
 		return fmt.Errorf("pkcs11key: opening session: %s", err)
 	}
-	ps.session = &session
+	ps.session = session
 
 	publicKeyID, err := ps.getPublicKeyID(ps.publicKey)
 	if err != nil {
-		ps.module.CloseSession(session)
+		ps.session.Close()
 		return fmt.Errorf("looking up public key: %s", err)
 	}
 
 	// Fetch the private key by matching its id to the public key handle.
-	privateKeyHandle, err := ps.getPrivateKey(ps.module, session, publicKeyID)
+	privateKeyObject, err := ps.getPrivateKey(publicKeyID)
 	if err != nil {
-		ps.module.CloseSession(session)
+		ps.session.Close()
 		return fmt.Errorf("getting private key: %s", err)
 	}
-	ps.privateKeyHandle = privateKeyHandle
+	ps.privateKey = p11.PrivateKey(privateKeyObject)
 	return nil
 }
 
 // getPrivateKey gets a handle to the private key whose CKA_ID matches the
-// provided publicKeyID. It must be called with the ps.sessionMu lock held.
-func (ps *Key) getPrivateKey(module ctx, session pkcs11.SessionHandle, publicKeyID []byte) (pkcs11.ObjectHandle, error) {
-	var noHandle pkcs11.ObjectHandle
-	privateKeyHandle, err := ps.findObject([]*pkcs11.Attribute{
+// provided publicKeyID.
+func (ps *Key) getPrivateKey(publicKeyID []byte) (p11.Object, error) {
+	privateKey, err := ps.session.FindObject([]*pkcs11.Attribute{
 		pkcs11.NewAttribute(pkcs11.CKA_CLASS, pkcs11.CKO_PRIVATE_KEY),
 		pkcs11.NewAttribute(pkcs11.CKA_ID, publicKeyID),
 	})
 	if err != nil {
-		return noHandle, err
+		return p11.Object{}, err
 	}
 
 	// Check whether the key has the CKA_ALWAYS_AUTHENTICATE attribute.
-	// If so, fail: we don't want to have to re-authenticate for each sign
-	// operation.
-	attributes, err := module.GetAttributeValue(session, privateKeyHandle, []*pkcs11.Attribute{
-		pkcs11.NewAttribute(pkcs11.CKA_ALWAYS_AUTHENTICATE, false),
-	})
-	// The PKCS#11 spec states that C_GetAttributeValue may return
-	// CKR_ATTRIBUTE_TYPE_INVALID if an object simply does not posses a given
-	// attribute. We don't consider that an error: the absence of the
-	// CKR_ATTRIBUTE_TYPE_INVALID property is just fine.
-	if err != nil && err == pkcs11.Error(pkcs11.CKR_ATTRIBUTE_TYPE_INVALID) {
-		return privateKeyHandle, nil
-	} else if err != nil {
-		return noHandle, err
+	// If so, set the ps.alwaysAuthenticate bit so we know to Login again after
+	// each private key operation.
+	alwaysAuthenticate, err := privateKey.Attribute(pkcs11.CKA_ALWAYS_AUTHENTICATE)
+	if err != nil {
+		return p11.Object{}, err
 	}
-	if len(attributes) > 0 && len(attributes[0].Value) > 0 && attributes[0].Value[0] == 1 {
+	if len(alwaysAuthenticate) > 0 && alwaysAuthenticate[0] == 1 {
 		ps.alwaysAuthenticate = true
 	}
 
-	return privateKeyHandle, nil
+	return privateKey, nil
 }
 
 // Destroy tears down a Key by closing the session. It should be
@@ -313,9 +232,7 @@ func (ps *Key) Destroy() error {
 		// time they try to sign something. It's also unnecessary to log out explicitly:
 		// module.CloseSession will log out once the last session in the application is
 		// closed.
-		ps.sessionMu.Lock()
-		defer ps.sessionMu.Unlock()
-		err := ps.module.CloseSession(*ps.session)
+		err := ps.session.Close()
 		ps.session = nil
 		if err != nil {
 			return fmt.Errorf("pkcs11key: close session: %s", err)
@@ -324,46 +241,43 @@ func (ps *Key) Destroy() error {
 	return nil
 }
 
-func (ps *Key) openSession() (pkcs11.SessionHandle, error) {
-	var noSession pkcs11.SessionHandle
-	slots, err := ps.module.GetSlotList(true)
+func (ps *Key) openSession() (p11.Session, error) {
+	slots, err := ps.module.Slots()
 	if err != nil {
-		return noSession, err
+		return nil, err
 	}
 
 	for _, slot := range slots {
-		// Check that token label matches.
-		tokenInfo, err := ps.module.GetTokenInfo(slot)
+		tokenInfo, err := slot.TokenInfo()
 		if err != nil {
-			return noSession, err
+			return nil, err
 		}
 		if tokenInfo.Label != ps.tokenLabel {
 			continue
 		}
 
-		// Open session
-		session, err := ps.module.OpenSession(slot, pkcs11.CKF_SERIAL_SESSION)
+		session, err := slot.OpenSession()
 		if err != nil {
-			return session, err
+			return nil, err
 		}
 
 		// Login
 		// Note: Logged-in status is application-wide, not per session. But in
 		// practice it appears to be okay to login to a token multiple times with the same
 		// credentials.
-		if err = ps.module.Login(session, pkcs11.CKU_USER, ps.pin); err != nil {
+		if err = session.Login(p11.RegularUser, ps.pin); err != nil {
 			if err == pkcs11.Error(pkcs11.CKR_USER_ALREADY_LOGGED_IN) {
 				// But if the token says we're already logged in, it's ok.
 				err = nil
 			} else {
-				ps.module.CloseSession(session)
-				return session, err
+				session.Close()
+				return nil, err
 			}
 		}
 
 		return session, err
 	}
-	return noSession, fmt.Errorf("no slot found matching token label %q", ps.tokenLabel)
+	return nil, fmt.Errorf("no slot found matching token label %q", ps.tokenLabel)
 }
 
 // Public returns the public key for the PKCS #11 key.
@@ -372,9 +286,7 @@ func (ps *Key) Public() crypto.PublicKey {
 }
 
 // Sign performs a signature using the PKCS #11 key.
-func (ps *Key) Sign(rand io.Reader, msg []byte, opts crypto.SignerOpts) (signature []byte, err error) {
-	ps.sessionMu.Lock()
-	defer ps.sessionMu.Unlock()
+func (ps *Key) Sign(rand io.Reader, msg []byte, opts crypto.SignerOpts) ([]byte, error) {
 	if ps.session == nil {
 		return nil, errors.New("pkcs11key: session was nil")
 	}
@@ -388,12 +300,12 @@ func (ps *Key) Sign(rand io.Reader, msg []byte, opts crypto.SignerOpts) (signatu
 	// state, we take a global lock while we do the logout and login, and during
 	// the signing.
 	if ps.alwaysAuthenticate {
-		modulesMu.Lock()
-		defer modulesMu.Unlock()
-		if err := ps.module.Logout(*ps.session); err != nil {
+		appMu.Lock()
+		defer appMu.Unlock()
+		if err := ps.session.Logout(); err != nil {
 			return nil, fmt.Errorf("pkcs11key: logout: %s", err)
 		}
-		if err = ps.module.Login(*ps.session, pkcs11.CKU_USER, ps.pin); err != nil {
+		if err := ps.session.Login(p11.RegularUser, ps.pin); err != nil {
 			return nil, fmt.Errorf("pkcs11key: login: %s", err)
 		}
 	}
@@ -402,39 +314,29 @@ func (ps *Key) Sign(rand io.Reader, msg []byte, opts crypto.SignerOpts) (signatu
 	hash := opts.HashFunc()
 	hashLen := hash.Size()
 	if len(msg) != hashLen {
-		err = fmt.Errorf("pkcs11key: input size does not match hash function output size: %d vs %d", len(msg), hashLen)
-		return
+		return nil, fmt.Errorf(
+			"pkcs11key: input size does not match hash function output size: %d vs %d",
+			len(msg), hashLen)
 	}
 
 	// Add DigestInfo prefix
-	var mechanism []*pkcs11.Mechanism
+	var mechanism *pkcs11.Mechanism
 	var signatureInput []byte
 
 	switch ps.publicKey.(type) {
 	case *rsa.PublicKey:
-		mechanism = []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_RSA_PKCS, nil)}
+		mechanism = pkcs11.NewMechanism(pkcs11.CKM_RSA_PKCS, nil)
 		prefix, ok := hashPrefixes[hash]
 		if !ok {
-			err = errors.New("pkcs11key: unknown hash function")
-			return
+			return nil, errors.New("pkcs11key: unknown hash function")
 		}
 		signatureInput = append(prefix, msg...)
 	case *ecdsa.PublicKey:
-		mechanism = []*pkcs11.Mechanism{pkcs11.NewMechanism(pkcs11.CKM_ECDSA, nil)}
+		mechanism = pkcs11.NewMechanism(pkcs11.CKM_ECDSA, nil)
 		signatureInput = msg
 	default:
 		return nil, fmt.Errorf("unrecognized key type %T", ps.publicKey)
 	}
 
-	// Perform the sign operation
-	err = ps.module.SignInit(*ps.session, mechanism, ps.privateKeyHandle)
-	if err != nil {
-		return nil, fmt.Errorf("pkcs11key: sign init: %s", err)
-	}
-
-	signature, err = ps.module.Sign(*ps.session, signatureInput)
-	if err != nil {
-		return nil, fmt.Errorf("pkcs11key: sign: %s", err)
-	}
-	return
+	return ps.privateKey.Sign(*mechanism, signatureInput)
 }
