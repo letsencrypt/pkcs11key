bcbf5f6d4527f47c07fa901f31e8cd3178e3aa46
Matthew McPherrin <github@mcpherrin.ca>
2025-12-03 13:41:19 -0500

    Remove v2 code (#39)
    pkcs11key has switched to the module-enabled v4 a long time ago. The v2
    code at the top level should be removed.
    
    We cannot tag any more v2 releases after this, but we aren't planning
    to.
    
    Fixes #35

--- doc.go
+++
@@ -1,6 +0,0 @@
-package pkcs11key
-
-// This package represents "v2" of the pkcs11key package, which was not
-// compatible with Go modules and is now frozen in amber. You should instead
-// import github.com/letsencrypt/pkcs11key/v3, which has the same interface but
-// will be kept up-to-date and is compatible with Go modules.
